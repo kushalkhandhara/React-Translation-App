3a331a2e6e4332f76eb21d78934957847d90fa24
Kushal Khandhara <kkhandhara@gmail.com>
2024-05-29 16:40:50 +0530

    First Changes

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import { Trans, useTranslation } from 'react-i18next';
+// import LanguageSelector from './components/language-selector';
+import "./App.css";
+import Translate from './components/Translate';
+
+// React Libraries I have used 
+// react-i18next
+// i18next-browser-languagedetector
+// i18next-http-backend
+
+
+function App() {
+
+ 
+  return (
+  <>
+    <Translate/>
+  </>
+  );
+}
+
+export default App;
